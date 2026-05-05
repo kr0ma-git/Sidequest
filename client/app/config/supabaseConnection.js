5b4d38150f0c932d53c508b8fceb1975090f5f83
kr0ma-git <w4ynekousuke@gmail.com>
2026-05-05 12:31:25 +0800

    feat: added the auth configuration for the client side supabase connection and enabling AsynchStorage

--- client/app/config/supabaseConnection.js
+++ client/app/config/supabaseConnection.js
@@ -1,3 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const supabase = createClient(process.env.EXPO_PUBLIC_SUPABASE_URL, process.env.EXPO_PUBLIC_SUPABASE_PERISHABLE_KEY);
+export const supabase = createClient(process.env.EXPO_PUBLIC_SUPABASE_URL, process.env.EXPO_PUBLIC_SUPABASE_PERISHABLE_KEY, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
